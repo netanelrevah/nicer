ba3c0a84f1fbbc33d7ac550ba1c2471e69a20e27
Netanel Revah <netanelrevah@outlook.com>
2016-12-07 22:52:30 +0200

    add timestamp object

--- nicer/times.py
+++ nicer/times.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
+import time
 
 import pytz
+
+from nicer.eqs import ComparableMixin
 
 __author__ = 'netanelrevah'
 
@@ -39,3 +42,44 @@ def datetime_from_timestamp(value):
 
 def datetime_from_seconds_and_microseconds(seconds, microseconds):
     return datetime.fromtimestamp(seconds, pytz.UTC) + timedelta(microseconds=microseconds)
+
+
+class Timestamp(ComparableMixin):
+    def __init__(self, seconds, second_parts=0):
+        self.seconds = seconds
+        self.seconds_parts = second_parts
+
+    @staticmethod
+    def now(precision=9):
+        return Timestamp.from_timestamp(time.time(), precision)
+
+    @staticmethod
+    def from_timestamp(timestamp, precision=9):
+        return Timestamp(int(timestamp), int((timestamp % 1) * (10 ** precision)))
+
+    @staticmethod
+    def from_datetime(dt):
+        return Timestamp(seconds_from_datetime(dt), microseconds_from_datetime(dt))
+
+    def __lt__(self, other):
+        return not (self >= other)
+
+    def __le__(self, other):
+        if isinstance(other, Timestamp):
+            return ((self == other)
+                    or (self.seconds < other.seconds)
+                    or (self.seconds == other.seconds and self.seconds_parts > other.seconds_parts))
+        raise TypeError()
+
+    def __gt__(self, other):
+        return not (self <= other)
+
+    def __ge__(self, other):
+        if isinstance(other, Timestamp):
+            return ((self == other)
+                    or (self.seconds > other.seconds)
+                    or (self.seconds == other.seconds and self.seconds_parts > other.seconds_parts))
+        raise TypeError()
+
+    def to_comparable(self):
+        return self.seconds, self.seconds_parts
